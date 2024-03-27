336ae5dceb24bd3f8771e3bb721746518adef121
martalionaite <153713084+martalionaite@users.noreply.github.com>
2024-03-27 13:41:11 +0200

    useEffect

--- src/pages/PageStudents.jsx
+++ src/pages/PageStudents.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import { StudentList } from "../components/students/StudentList";
 import { StudentsNoData } from "../components/students/StudentsNoData";
 
 export function PageStudents() {
+    const [studentData, setStudentData] = useState([]);
+        const dataURL = 'https://raw.githubusercontent.com/martalionaite/48-grupe-react-form/main/public/students.json';
+
+        useEffect(() => {
+            fetch(dataURL)
+                .then(res => res.jason())
+                .then(data =>setStudentData(data))
+                .catch( e => console.error(e));
+        }, []);
     return (
         <>
             <h1 className="page-title">Students</h1>
             <p className="page-description">Students attending this class:</p>
-            <StudentsNoData />
-            <StudentList />
+            {studentData.length === 0 ? <StudentsNoData /> : <StudentList />}
         </>
-    )
+    );
 }
