04b3019308d817270c9dbc72b8aa2b96ace4bf69
martalionaite <153713084+martalionaite@users.noreply.github.com>
2024-03-29 09:15:52 +0200

    form

--- src/components/form/RegisterForm.jsx
+++ src/components/form/RegisterForm.jsx
@@ -1,23 +1,117 @@
+import { useState } from 'react';
 import style from './Form.module.css';
 
 export function RegisterForm() {
+    const [username, setUsername] = useState('');
+    const [usernameErr, setUsernameErr] = useState('');
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+
+    function handleUsernameChange(e) {
+        setUsername(e.target.value);
+    }
+
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+    }
+
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+    }
+
+    function handleRepeatPasswordChange(e) {
+        setRepeatPassword(e.target.value);
+    }
+
+    function isValidUsername(text) {
+        if (text.length < 1) {
+            return 'Per trumpas';
+        }
+
+        if (text.length > 20) {
+            return 'Per ilgas';
+        }
+
+        return true;
+    }
+
+    function isValidEmail(text) {
+        if (text.length < 6) {
+            return 'Per trumpas';
+        }
+
+        if (text.length > 50) {
+            return 'Per ilgas';
+        }
+
+        return true;
+    }
+
+    function isValidPassword(text) {
+        if (text.length < 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    function handleFormSubmit(e) {
+        e.preventDefault();
+
+        const usernameErrorValue = isValidUsername(username);
+        let isAllFormValid = true;
+
+        if (usernameErrorValue !== true) {
+            isAllFormValid = false;
+            setUsernameErr(usernameErrorValue);
+        } else {
+            setUsernameErr('');
+        }
+
+        if (!isValidEmail(email)) {
+            isAllFormValid = false;
+            console.log('blogas email');
+        }
+
+        if (!isValidPassword(password)) {
+            isAllFormValid = false;
+            console.log('blogas password');
+        }
+
+        if (password !== repeatPassword) {
+            isAllFormValid = false;
+            console.log('blogas repeat password');
+        }
+
+        if (isAllFormValid) {
+            console.log('viskas gerai, siuncia info i serveri');
+        }
+    }
+
     return (
-        <form className={style.form}>
+        <form onSubmit={handleFormSubmit} className={style.form}>
             <div className={style.formRow}>
                 <label className={style.label} htmlFor="">Username</label>
-                <input className={style.input} type="text" placeholder="Eg. John" />
+                <input value={username} onChange={handleUsernameChange} className={style.input} type="text" placeholder="Eg. John" />
+                {usernameErr.length === 0 ? null : <p className={style.error}>{usernameErr}</p>}
+                {usernameErr && <p className={style.error}>{usernameErr}</p>}
             </div>
             <div className={style.formRow}>
                 <label className={style.label} htmlFor="">Email</label>
-                <input className={style.input} type="email" placeholder="Eg. John@cena.com" />
+                <input value={email} onChange={handleEmailChange} className={style.input} type="email" placeholder="Eg. john@cena.com" />
+                <p className={style.error}>Error...</p>
             </div>
             <div className={style.formRow}>
                 <label className={style.label} htmlFor="">Password</label>
-                <input className={style.input} type="password" placeholder="youcantseemenow" />
+                <input value={password} onChange={handlePasswordChange} className={style.input} type="password" placeholder="enter your password" />
+                <p className={style.error}>Error...</p>
             </div>
             <div className={style.formRow}>
                 <label className={style.label} htmlFor="">Repeat password</label>
-                <input className={style.input} type="password" placeholder="youcantseemenow" />
+                <input value={repeatPassword} onChange={handleRepeatPasswordChange} className={style.input} type="password" placeholder="enter your password" />
+                <p className={style.error}>Error...</p>
             </div>
             <div className={style.formRow}>
                 <button className={style.button} type="submit">Register</button>
